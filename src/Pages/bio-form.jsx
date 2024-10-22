2f5fa371a0ccff6a093fbe8f6a7f25e6bf1a1080
JamiRazaCodes <jamiraxa234@gmail.com>
2024-10-22 03:49:48 -0700

    error solved

--- src/Pages/bio-form.jsx
+++ src/Pages/bio-form.jsx
@@ -17,7 +17,7 @@ function BioForm() {
 
   const navigate = useNavigate();
 
-  s
+  
   const handleChange = (e) => {
     const { id, value } = e.target;
     setFormData({ ...formData, [id]: value });
